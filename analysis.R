a3c1023227dcc11b4f8c8a58216d278707593475
Locksley <41974140+LocksleyLK@users.noreply.github.com>
2020-03-02 13:28:08 -0800

    Update analysis.R

--- analysis.R
+++ analysis.R
@@ -1,8 +1,9 @@
 
-# Reddit Tech Salary DataFrame
+# Reddit Tech Salary DataFrame.
 
 tech_salary <- read.csv("reddit_tech_salary_sheet.csv", stringsAsFactors = FALSE)
 
+# Wrangling dataframe to only include necessary columns.
 tech_salary <- tech_salary %>%
   select(
     Company.Name, 
@@ -13,15 +14,17 @@ tech_salary <- tech_salary %>%
     Gender
   )
 
+# Creating a small sample of the dataframe.
 tech_salary_sample <- tech_salary[2:4, ]
 
-# Individual Wealth Dataframe
 
-
+# Individual Wealth Dataframe.
 individual_wealth <- read.csv("individual wealth.csv", stringsAsFactors = FALSE)
 
+# Only including one variable.
 individual_wealth <- individual_wealth[individual_wealth$Variable == "Mean net wealth per person (current prices)", ]
 
+# Wrangling to only the necessary columns.
 individual_wealth <- individual_wealth %>%
   select(
     Country,
@@ -30,40 +33,45 @@ individual_wealth <- individual_wealth %>%
     Value
   )
 
+# Creating a small sample of the dataframe.
 individual_wealth_sample <- individual_wealth[1:3, ]
 
 
 # For our tech slararies data table, the main features we focused on was 
-#salary in relation to highest level of education and gender.
+# salary in relation to highest level of education and gender.
 
-# The summary of just tech salary is:
+# Removing all salaries that are null, greater than $250000 and less than $10000.
 tech_salary_shrunk <- tech_salary[tech_salary$Total.Base.Salary.in.2018..in.USD. < 250000, ]
 tech_salary_shrunk <- tech_salary_shrunk[tech_salary_shrunk$Total.Base.Salary.in.2018..in.USD. > 10000, ]
 tech_salary_shrunk <- tech_salary_shrunk[2:nrow(tech_salary_shrunk), ]
 
 summary(tech_salary_shrunk$Total.Base.Salary.in.2018..in.USD.)
 
+# Including only male and female genders.
 tech_salary_gender <- tech_salary_shrunk[tech_salary_shrunk$Gender == "Male" | tech_salary_shrunk$Gender == "Female", ]
 
-
+# Getting a summary of the female data.
 tech_female <- tech_salary_shrunk[tech_salary_shrunk$Gender == "Female", ]
 summary(tech_female$Total.Base.Salary.in.2018..in.USD.)
 
+# Getting a summary of the male data.
 tech_male <- tech_salary_shrunk[tech_salary_shrunk$Gender == "Male", ]
 summary(tech_male$Total.Base.Salary.in.2018..in.USD.)
 
+# Creating the first gender plot.
 gender_plot <- ggplot(data = tech_salary_gender) +
   geom_boxplot(
     mapping = aes(x = Gender, y = Total.Base.Salary.in.2018..in.USD.)
   )
 
-
+# Wrangling the education to include the top four degrees.
 tech_salary_education <- tech_salary_shrunk[tech_salary_shrunk$Highest.Level.of.Formal.Education.Completed == "High School" |
                                            tech_salary_shrunk$Highest.Level.of.Formal.Education.Completed == "Undergraduate Degree" |
                                            tech_salary_shrunk$Highest.Level.of.Formal.Education.Completed == "Graduate Degree" |
                                            tech_salary_shrunk$Highest.Level.of.Formal.Education.Completed == "Doctorate or Post-doctorate",
                                          ]
 
+# The below code creates a summary of all the education relative to gender.
 tech_highschool <- tech_salary_shrunk[tech_salary_shrunk$Highest.Level.of.Formal.Education.Completed == "High School",]
 summary(tech_highschool$Total.Base.Salary.in.2018..in.USD.)
 
@@ -141,6 +149,7 @@ lynzley_plot <- ggplot(data = salary_means) +
 
 
 # Locksley's Part 3
+# The below code gets a summary of all the salaries relative to education AND gender.
 tech_salary_shrunk_female <- tech_salary_shrunk[tech_salary_shrunk$Gender == "Female", ]
 # View(tech_salary_shrunk_female)
 tech_salary_shrunk_female <- tech_salary_shrunk_female[tech_salary_shrunk_female$Highest.Level.of.Formal.Education.Completed == "High School", ]
@@ -193,14 +202,14 @@ tech_salary_shrunk_female <- tech_salary_shrunk[tech_salary_shrunk$Gender == "Ma
 tech_salary_shrunk_female <- tech_salary_shrunk_female[tech_salary_shrunk_female$Highest.Level.of.Formal.Education.Completed == "Doctorate or Post-doctorate", ]
 summary(tech_salary_shrunk_female$Total.Base.Salary.in.2018..in.USD.)
 
-
+# Dataframe to utilize for plotting.
 tech_salaries <- data.frame(
   "Education" = c("1 - High School Diploma", "2 - Bachelor's Degree", "3 - Master's Degree", "4 - Doctorate"),
   "Male" = c(75256, 81280, 75826, 111570),
   "Female" = c(65119, 77830, 78086, 121000)
 )
 
-
+# Plot for data.
 locksley_plot <- ggplot(data = tech_salaries) +
   
   geom_point(
